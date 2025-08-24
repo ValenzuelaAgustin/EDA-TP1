ad52f386b42b56f79270ff7600f2e0e977f53e84
Agus <valenzuela.agustin.tomas@gmail.com>
2025-08-24 13:03:04 -0300

    .

---
+++ include/nmath.h
@@ -0,0 +1,8 @@
+#ifndef NMATH_H
+#define NMATH_H
+
+#define DOT_PRODUCT(v0, v1) ( (v0).x * (v1).x + (v0).y * (v1).y + (v0).z * (v1).z )
+
+float Q_rsqrt(float number);
+
+#endif
--- src/orbitalSim.cpp
+++ src/orbitalSim.cpp
@@ -9,6 +9,7 @@
 #define _USE_MATH_DEFINES
 
 #include "OrbitalSim.h"
+#include "nmath.h"
 #include <stdlib.h>
 #include <math.h>
 
@@ -66,11 +67,15 @@ float getRandomFloat(float min, float max)
  */
 OrbitalSim_t* constructOrbitalSim(float timeStep)
 {
-	// Your code goes here...
+	OrbitalSim_t* ptr = new OrbitalSim_t;
+	if (!ptr)
+		return NULL;
 
+	ptr->dt = timeStep;
+	ptr->bodyNum = SOLARSYSTEM_BODYNUM;
+	ptr->EphemeridesBody = solarSystem;
 
-
-	return NULL; // This should return your orbital sim
+	return ptr; // This should return your orbital sim
 }
 
 /**
@@ -78,9 +83,7 @@ OrbitalSim_t* constructOrbitalSim(float timeStep)
  */
 void destroyOrbitalSim(OrbitalSim_t* sim)
 {
-	// Your code goes here...
-
-
+	delete sim;
 }
 
 /**
@@ -90,7 +93,54 @@ void destroyOrbitalSim(OrbitalSim_t* sim)
  */
 void updateOrbitalSim(OrbitalSim_t* sim)
 {
-	// Your code goes here...
+	//if (!sim || !sim->EphemeridesBody || sim->bodyNum < 1 || sim->dt <= 0)
+	//	return;
 
+	int i, j;
+	Vector3 acceleration;
+	float inverse_distance_cubed;
 
+	for (i = 0; i < sim->bodyNum; i++)
+	{
+		sim->EphemeridesBody[i].acceleration.x = 0;
+		sim->EphemeridesBody[i].acceleration.y = 0;
+		sim->EphemeridesBody[i].acceleration.z = 0;
+	}
+
+	for (i = 0; i < sim->bodyNum - 1; i++)
+	{
+		for (j = i + 1; j < sim->bodyNum; j++)
+		{
+			acceleration.x = sim->EphemeridesBody[j].position.x - sim->EphemeridesBody[i].position.x;
+			acceleration.y = sim->EphemeridesBody[j].position.y - sim->EphemeridesBody[i].position.y;
+			acceleration.z = sim->EphemeridesBody[j].position.z - sim->EphemeridesBody[i].position.z;
+
+			inverse_distance_cubed = Q_rsqrt(DOT_PRODUCT(acceleration, acceleration));
+			inverse_distance_cubed = inverse_distance_cubed * inverse_distance_cubed * inverse_distance_cubed;
+			inverse_distance_cubed *= GRAVITATIONAL_CONSTANT;
+
+			acceleration.x *= inverse_distance_cubed;
+			acceleration.y *= inverse_distance_cubed;
+			acceleration.z *= inverse_distance_cubed;
+
+			sim->EphemeridesBody[i].acceleration.x += sim->EphemeridesBody[j].mass * acceleration.x;
+			sim->EphemeridesBody[i].acceleration.y += sim->EphemeridesBody[j].mass * acceleration.y;
+			sim->EphemeridesBody[i].acceleration.z += sim->EphemeridesBody[j].mass * acceleration.z;
+
+			sim->EphemeridesBody[j].acceleration.x += sim->EphemeridesBody[i].mass * acceleration.x;
+			sim->EphemeridesBody[j].acceleration.y += sim->EphemeridesBody[i].mass * acceleration.y;
+			sim->EphemeridesBody[j].acceleration.z += sim->EphemeridesBody[i].mass * acceleration.z;
+		}
+	}
+
+	for (i = 0; i < sim->bodyNum; i++)
+	{
+		sim->EphemeridesBody[i].velocity.x += sim->EphemeridesBody[i].acceleration.x * sim->dt;
+		sim->EphemeridesBody[i].velocity.y += sim->EphemeridesBody[i].acceleration.y * sim->dt;
+		sim->EphemeridesBody[i].velocity.z += sim->EphemeridesBody[i].acceleration.z * sim->dt;
+
+		sim->EphemeridesBody[i].position.x += sim->EphemeridesBody[i].velocity.x * sim->dt;
+		sim->EphemeridesBody[i].position.y += sim->EphemeridesBody[i].velocity.y * sim->dt;
+		sim->EphemeridesBody[i].position.z += sim->EphemeridesBody[i].velocity.z * sim->dt;
+	}
 }
